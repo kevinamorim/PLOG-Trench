75289e141306873e69eafb01f5de2de05f906107
ludwig801 <lmaga801@gmail.com>
2014-11-07 22:00:11 +0000

    Better gameover verifications.

--- src/game.pl
+++ src/game.pl
@@ -2,9 +2,10 @@
 
 :- consult(logic).
 :- consult(io).
+:- consult(draw).
 
 % play_game().
-play_game(_) :-
+play_game :-
         initialize(L),
         print_board(L),
         first_player(P), !,
@@ -28,12 +29,12 @@ read_player_move(L, Pl, NL) :-
         readPosition([A1,A2]),
         convert_alpha_num(A1, R),
         convert_alpha_num(A2, C),
-        getPiece(L, [R,C], P),
-        checkPiecePlayer(P, Pl), !,
+        get_piece(L, [R,C], P),
+        check_piece_player(P, Pl), !,
         write('Piece selected: '), get_board_symbol(P, S), write(S), nl,
         write('Select Target (xy): '),
         readPosition([B1,B2]),
-        movePiece(L, [A1,A2], [B1,B2], NL), !;
+        move_piece(L, [A1,A2], [B1,B2], NL), !;
         %else : happens when the player inserts coordinates of a piece that it's not his
         read_player_move(L, Pl, NL).
                     
@@ -63,14 +64,14 @@ initialize(X) :- game_list(X).
 %       - None of the pieces in the board has a valid move;
 %       - No pieces has been 'consumed' in 30 moves.
 game_over(GameList) :-
-        \+ player_has_pieces(GameList, p1), write('Player 2 wins'), nl;
-        \+ player_has_pieces(GameList, p2), write('Player 1 wins'), nl;
-        \+ player_has_moves(GameList, p1), write('Player 2 wins'), nl;
-        \+ player_has_moves(GameList, p2), write('Player 1 wins'), nl.
+        \+ player_has_pieces(GameList, p1), write('Player 2 wins'), !, nl;
+        \+ player_has_pieces(GameList, p2), write('Player 1 wins'), !, nl;
+        player_has_moves(GameList, p1, 1, 1), write('Player 2 wins'), !, nl;
+        player_has_moves(GameList, p2, 1, 1), write('Player 1 wins'), !, nl.
 
 % checks if the player has any piece remaining
 player_has_pieces(GameList, Player) :-
-        playerPieces(Player, L),
+        player_pieces(Player, L),
         player_has_pieces(GameList, L).
 
 player_has_pieces([_|_], []) :- fail.
@@ -79,40 +80,32 @@ player_has_pieces(GameList, [P|R]) :-
         member_matrix(P,GameList);
         player_has_pieces(GameList, R).
 
-% checks if the player has any piece with a valid move
-player_has_moves(GameList, Player) :-
-        player_has_moves(GameList, Player, 1, 1).
+player_has_moves(GameList, Player, X, Y) :-
+        X < 9, Y < 9,
+        get_piece(GameList, [X,Y], P),
+        check_piece_player(P, Player),
+        write('> Piece: '), write(P), nl,
+        piece_has_moves(GameList, [X,Y], [1,1]).
 
 player_has_moves(GameList, Player, X, Y) :-
-        %write('player_has_moves(GameList,'), write(Player), write(','),
-        %write(X), write(','), write(Y), write(')'), nl,
         X < 9, !,
                 X1 is X + 1,
-                player_has_moves(GameList, Player, X1, Y),
-                getPiece(GameList, [X,Y], P),
-                checkPiecePlayer(P, Player), !,
-                piece_has_moves(GameList, [X,Y], [1,1]);
+                player_has_moves(GameList, Player, X1, Y);
         Y < 9, !,
                 Y1 is Y + 1,
-                player_has_moves(GameList, Player, 1, Y1),
-                getPiece(GameList, [X,Y], P),
-                checkPiecePlayer(P, Player), !,
-                piece_has_moves(GameList, [X,Y], [1,1]).
+                player_has_moves(GameList, Player, 1, Y1);
+        fail.
 
-player_has_moves([_|_], _, _, _).
-
-piece_has_moves([_|_], [X|Y], [X|Y]) :- fail.
+piece_has_moves([_|_], [X,Y], [X,Y]) :- fail.
 
 piece_has_moves(GameList, [X,Y], [A1,A2]) :-
         A1 < 9, !,
                 B1 is A1 + 1,
                 piece_has_moves(GameList, [X,Y], [B1,A2]),
-                canMove(GameList, [X,Y], [A1,A2]);
+                can_move(GameList, [X,Y], [A1,A2]);
         A2 < 9, !,
                 B2 is A2 + 1,
                 piece_has_moves(GameList, [X,Y], [1,B2]);
-                canMove(GameList, [X,Y], [A1,A2]).
+                can_move(GameList, [X,Y], [A1,A2]).
 
 piece_has_moves([_|_], [_], [_]).
-
-               
