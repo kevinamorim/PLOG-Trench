7cff36f7959b9f6f4bfb65b832034f5a848733be
Kevin Amorim <kevinamorim4@gmail.com>
2014-11-09 20:42:06 +0000

    Fixed documentation and other small things.

--- src/game.pl
+++ src/game.pl
@@ -65,12 +65,16 @@ initialize(X) :- game_list(X).
 %       - None of the pieces in the board has a valid move;
 %       - No pieces has been 'consumed' in 30 moves.
 game_over(GameList) :-
+        
+        
         \+ player_has_pieces(GameList, p1),
                 write('Player 1 has no pieces left.'), nl,
                 write('Player 2 wins'), !, nl;
         \+ player_has_pieces(GameList, p2),
                 write('Player 2 has no pieces left.'), nl,
                 write('Player 1 wins'), !, nl;
+        
+        
         \+ player_has_moves(GameList, p1, a, i),
                 write('Player 1 has no moves left.'), nl,
                 write('Player 2 wins'), !, nl;
@@ -78,18 +82,29 @@ game_over(GameList) :-
                 write('Player 2 has no moves left.'), nl,
                 write('Player 1 wins'), !, nl.
 
+% ==============================================
 % checks if the player has any piece remaining
+% ==============================================
+% Prepares checking
 player_has_pieces(GameList, Player) :-
+        % Get player pieces list
         player_pieces(Player, L),
         player_has_pieces(GameList, L).
 
+% Fail case
 player_has_pieces([_|_], []) :- fail.
 
+% For each piece checks if it exists in the gamelist. If any is found, returns yes.
 player_has_pieces(GameList, [P|R]) :-
         member_matrix(P,GameList);
         player_has_pieces(GameList, R).
+% ==============================================
+% ==============================================
 
+% ==============================================
 % checks if the player has any pieces width a valid move
+% ==============================================
+% 
 player_has_moves(GameList, Player, X, Y) :-
         convert_alpha_num(X, X1),
         convert_alpha_num(Y, Y1),
